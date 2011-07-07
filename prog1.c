908e647b1683a77660e234625086b89943c220a1
Tomokatsu Mizukusa <mizukusa.tomokatsu@gmail.com>
2011-07-07 17:32:14 +0900

    Lesson1.2: Add Line

--- prog1.c
+++ prog1.c
@@ -3,6 +3,12 @@
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_LINE_LOOP);
+  glVertex2d(-0.9, -0.9);
+  glVertex2d(0.9, -0.9);
+  glVertex2d(0.9, 0.9);
+  glVertex2d(-0.9, 0.9);
+  glEnd();
   glFlush();
 }
 
