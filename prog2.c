2614d4ec653bb7cc7ffae0b29e2c4697d2c6cf3e
Tomokatsu Mizukusa <mizukusa.tomokatsu@gmail.com>
2011-07-08 04:50:30 +0900

    Add prog2.c

---
+++ prog2.c
@@ -0,0 +1,33 @@
+#include <GL/glut.h>
+
+void display()
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_POLYGON);
+  glColor3d(1.0, 0.0, 0.0);  // Red
+  glVertex2d(-0.9, -0.9);
+  glColor3d(0.0, 1.0, 0.0);  // Green
+  glVertex2d(0.9, -0.9);
+  glColor3d(0.0, 0.0, 1.0);  // Blue
+  glVertex2d(0.9, 0.9);
+  glColor3d(1.0, 1.0, 0.0);  // Yellow
+  glVertex2d(-0.9, 0.9);
+  glEnd();
+  glFlush();
+}
+
+void init()
+{
+  glClearColor(1.0, 1.0, 1.0, 1.0);
+}
+
+int main(int argc, char** argv)
+{
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGBA);
+  glutCreateWindow(argv[0]);
+  glutDisplayFunc(display);
+  init();
+  glutMainLoop();
+  return 0;
+}
