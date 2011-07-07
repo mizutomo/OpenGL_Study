524465fcfd4d08fff21a23f9c21dab0125c6b5c9
Tomokatsu Mizukusa <mizukusa.tomokatsu@gmail.com>
2011-07-08 08:12:13 +0900

    Add Cubic

--- prog2.c
+++ prog2.c
@@ -1,20 +1,53 @@
 #include <GL/glut.h>
 
+GLdouble vertex[][3] = {
+  {0.0, 0.0, 0.0},
+  {1.0, 0.0, 0.0},
+  {1.0, 1.0, 0.0},
+  {0.0, 1.0, 0.0},
+  {0.0, 0.0, 1.0},
+  {1.0, 0.0, 1.0},
+  {1.0, 1.0, 1.0},
+  {0.0, 1.0, 1.0}
+};
+
+int edge[][2] = {
+  {0, 1},
+  {1, 2},
+  {2, 3},
+  {3, 0},
+  {4, 5},
+  {5, 6},
+  {6, 7},
+  {7, 4},
+  {0, 4},
+  {1, 5},
+  {2, 6},
+  {3, 7}
+};
+
 void display()
 {
+  int i;
+
   glClear(GL_COLOR_BUFFER_BIT);
-  glRotated(25.0, 0.0, 1.0, 0.0);
-  glBegin(GL_POLYGON);
-  glColor3d(1.0, 0.0, 0.0);  // Red
-  glVertex2d(-0.9, -0.9);
-  glColor3d(0.0, 1.0, 0.0);  // Green
-  glVertex2d(0.9, -0.9);
-  glColor3d(0.0, 0.0, 1.0);  // Blue
-  glVertex2d(0.9, 0.9);
-  glColor3d(1.0, 1.0, 0.0);  // Yellow
-  glVertex2d(-0.9, 0.9);
+
+  glColor3d(0.0, 0.0, 0.0);
+  glBegin(GL_LINES);
+  for (i = 0; i < 12; i++) {
+    glVertex3dv(vertex[edge[i][0]]);
+    glVertex3dv(vertex[edge[i][1]]);
+  }
   glEnd();
   glFlush();
+}
+
+void resize(int w, int h)
+{
+  glViewport(0, 0, w, h);
+
+  glLoadIdentity();
+  glOrtho(-2.0, 2.0, -2.0, 2.0, -2.0, 2.0);
 }
 
 void init()
@@ -28,6 +61,7 @@ int main(int argc, char** argv)
   glutInitDisplayMode(GLUT_RGBA);
   glutCreateWindow(argv[0]);
   glutDisplayFunc(display);
+  glutReshapeFunc(resize);
   init();
   glutMainLoop();
   return 0;
