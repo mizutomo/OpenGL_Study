d0c981f90ed16a8bdba47279576d3527a9694221
Tomokatsu Mizukusa <mizukusa.tomokatsu@gmail.com>
2011-07-07 17:40:06 +0900

    Lesson1.5: Change over color to rainbow

--- prog1.c
+++ prog1.c
@@ -3,11 +3,14 @@
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT);
-  glColor3d(1.0, 0.0, 0.0);
   glBegin(GL_POLYGON);
+  glColor3d(1.0, 0.0, 0.0);  // Red
   glVertex2d(-0.9, -0.9);
+  glColor3d(0.0, 1.0, 0.0);  // Green
   glVertex2d(0.9, -0.9);
+  glColor3d(0.0, 0.0, 1.0);  // Blue
   glVertex2d(0.9, 0.9);
+  glColor3d(1.0, 1.0, 0.0);  // Yellow
   glVertex2d(-0.9, 0.9);
   glEnd();
   glFlush();
