d74633321e6badc97f750dc58cb4f8fc07073a58
Tomokatsu Mizukusa <mizukusa.tomokatsu@gmail.com>
2011-07-07 17:27:50 +0900

    Lesson1.1

--- prog1.c
+++ prog1.c
@@ -1,13 +1,23 @@
 #include <GL/glut.h>
 
 void display()
-{}
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glFlush();
+}
+
+void init()
+{
+  glClearColor(0.0, 0.0, 1.0, 1.0);
+}
 
 int main(int argc, char** argv)
 {
   glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_RGBA);
   glutCreateWindow(argv[0]);
   glutDisplayFunc(display);
+  init();
   glutMainLoop();
   return 0;
 }
