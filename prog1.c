170eca59c0135e7a5767bcace8eb5774a35c1940
Tomokatsu Mizukusa <mizukusa.tomokatsu@gmail.com>
2011-07-08 05:21:34 +0900

    Add point

--- prog1.c
+++ prog1.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <GL/glut.h>
 
+#define MAXPOINTS (100)
+GLint point[MAXPOINTS][2];
+int pointnum = 0;
+
 void display()
 {
+  int i;
+
   glClear(GL_COLOR_BUFFER_BIT);
+
+  if (pointnum > 1) {
+    glColor3d(0.0, 0.0, 0.0);
+    glBegin(GL_LINES);
+    for (i = 0; i < pointnum; ++i) {
+      glVertex2iv(point[i]);
+    }
+    glEnd();
+  }
+
   glFlush();
 }
 
@@ -16,21 +32,19 @@ void resize(int w, int h)
 
 void mouse(int button, int state, int x, int y)
 {
-  static int x0, y0;
-
   switch(button) {
     case GLUT_LEFT_BUTTON:
+      point[pointnum][0] = x;
+      point[pointnum][1] = y;
       if (state == GLUT_UP) {
         glColor3d(0.0, 0.0, 0.0);
         glBegin(GL_LINES);
-        glVertex2i(x0, y0);
-        glVertex2i(x, y);
+        glVertex2iv(point[pointnum - 1]);
+        glVertex2iv(point[pointnum]);
         glEnd();
         glFlush();
-      } else {
-        x0 = x;
-        y0 = y;
       }
+      if (pointnum < MAXPOINTS - 1) ++pointnum;
       break;
     case GLUT_MIDDLE_BUTTON:
       break;
